918f07c3caf071a506eccdbf2a8409a56126f5a2
romanandreev <anra@bk.ru>
2021-01-10 23:02:55 +0300

    Добавили менеджер для вывода последних продуктов

--- mainapp/models.py
+++ mainapp/models.py
@@ -16,6 +16,31 @@ User = get_user_model()
 # 7. Specification
 
 
+class LatestProductsManager:
+
+    @staticmethod
+    def get_products_for_main_page(*args, **kwargs):
+        with_respect_to = kwargs.get('with_respect_to')
+        products = []
+        ct_models = ContentType.objects.filter(model__in=args)
+        for ct_model in ct_models:
+            model_product = ct_model.model_class()._base_manager.all().order_by('-id')[:5]
+            products.extend(model_product)
+        if with_respect_to:
+            ct_model = ContentType.objects.filter(model=with_respect_to)
+            if ct_model.exists():
+                if with_respect_to in args:
+                    return sorted(
+                        products, key=lambda x: x.__class__._meta.model_name.startswith(with_respect_to), reverse=True
+                    )
+        return products
+
+class LatestProduct:
+
+    objects = LatestProductsManager()
+
+
+
 class Category(models.Model):
 
     name = models.CharField(max_length=255, verbose_name='Имя категории')
