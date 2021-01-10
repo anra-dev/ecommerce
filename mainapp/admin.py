4041b2a043da428a46578876625e12c497fa6dfd
romanandreev <anra@bk.ru>
2021-01-10 18:26:07 +0400

    Модели созданы и зарегистрированы в админке

--- mainapp/admin.py
+++ mainapp/admin.py
@@ -1,3 +1,35 @@
+from django import forms
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+
+class NotebookCategoryChoiceField(forms.ModelChoiceField):
+    pass
+
+class NotebookAdmin(admin.ModelAdmin):
+
+    def formfield_for_foreignkey(self, db_field, request, **kwargs):
+        if db_field.name == 'category':
+            return NotebookCategoryChoiceField(Category.objects.filter(slug='Notebooks'))
+        return super().formfield_for_foreignkey(db_field, request, **kwargs)
+
+
+class SmartphoneCategoryChoiceField(forms.ModelChoiceField):
+    pass
+
+class SmartphoneAdmin(admin.ModelAdmin):
+
+    def formfield_for_foreignkey(self, db_field, request, **kwargs):
+        if db_field.name == 'category':
+            return SmartphoneCategoryChoiceField(Category.objects.filter(slug='Smartphones'))
+        return super().formfield_for_foreignkey(db_field, request, **kwargs)
+
+
+admin.site.register(Category)
+admin.site.register(Notebook, NotebookAdmin)
+admin.site.register(Smartphone, SmartphoneAdmin)
+admin.site.register(CardProduct)
+admin.site.register(Card)
+admin.site.register(Customer)
+
+
