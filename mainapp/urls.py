ce8830c69428a97350765c0ae422350e2e227d60
romanandreev <anra@bk.ru>
2021-01-13 23:40:09 +0300

    Товары можно добавлять в корзину

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     path('product/<str:ct_model>/<str:slug>/', ProductDetailView.as_view(), name='product_detail'),
     path('category/<str:slug>/', CategoryDetailView.as_view(), name='category_detail'),
     path('cart/', CartView.as_view(), name='cart'),
-    path('add-to-cart/<str:ct_model>/<str:slug>/', AddToCartView.as_view, name='add_to_cart')
+    path('add-to-cart/<str:ct_model>/<str:slug>/', AddToCartView.as_view(), name='add_to_cart')
 ]
--- mainapp/views.py
+++ mainapp/views.py
@@ -1,21 +1,25 @@
 from django.views.generic import DetailView, View
+from django.contrib.contenttypes.models import ContentType
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
 
-from .models import Notebook, Smartphone, Category, LatestProduct, Customer, Cart
+from .models import Notebook, Smartphone, Category, LatestProduct, Customer, Cart, CartProduct
 from .mixins import CategoryDetailMixin
 
 
 class BaseView(View):
 
     def get(self, request, *args, **kwargs):
+        customer = Customer.objects.get(user=request.user)
+        cart = Cart.objects.get(owner=customer)
         categories = Category.objects.get_categories_for_left_sidebar()
         products = LatestProduct.objects.get_products_for_main_page(
             'notebook', 'smartphone', with_respect_to='notebook'
         )
         context = {
             'categories': categories,
-            'products': products
+            'products': products,
+            'cart': cart
         }
         return render(request, 'base.html', context)
 
@@ -37,7 +41,7 @@ class ProductDetailView(CategoryDetailMixin, DetailView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data()
-        context['ct-model'] = self.model._meta.model_name
+        context['ct_model'] = self.model._meta.model_name
         return context
 
 
@@ -53,8 +57,15 @@ class CategoryDetailView(CategoryDetailMixin, DetailView):
 class AddToCartView(View):
 
     def get(self, request, *args, **kwargs):
-        print(kwargs.get('ct_model'))
-        print(kwargs.get('slug'))
+        ct_model, product_slug = kwargs.get('ct_model'), kwargs.get('slug')
+        customer = Customer.objects.get(user=request.user)
+        cart = Cart.objects.get(owner=customer, in_order=False)
+        content_type = ContentType.objects.get(model=ct_model)
+        product = content_type.model_class().objects.get(slug=product_slug)
+        cart_product, created = CartProduct.objects.get_or_create(
+            user=customer, cart=cart, content_type=content_type, object_id=product.id, final_price=product.price
+        )
+        cart.products.add(cart_product)
         return HttpResponseRedirect('/cart/')
 
 
