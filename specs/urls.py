d549a6eac4c6e07ec74c954a04455ecae3adb9f9
romanandreev <anra@bk.ru>
2021-01-28 22:11:19 +0300

    Подгатовлены формы для категорий и характеристик, а так же их представления и шаблоны

--- specs/urls.py
+++ specs/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 
-from .views import BaseSpecView
+from .views import BaseSpecView, NewCategoryView, NewFeatureView
 
 urlpatterns = [
-    path('', BaseSpecView.as_view(), name='base-spec')
+    path('', BaseSpecView.as_view(), name='base-spec'),
+    path('new-category/', NewCategoryView.as_view(), name='new-category'),
+    path('new-feature/', NewFeatureView.as_view(), name='new-feature')
 ]
--- specs/views.py
+++ specs/views.py
@@ -1,8 +1,44 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponseRedirect
 from django.views import View
 
+from .forms import NewCategoryForm, NewCategoryFeatureKeyForm
 
 class BaseSpecView(View):
 
     def get(self, request, *args, **kwargs):
         return render(request, 'product_features.html', {})
+
+
+class NewCategoryView(View):
+
+    def get(self, request, *args, **kwargs):
+        form = NewCategoryForm(request.POST or None)
+        context = {'form': form}
+        return render(request, 'new_category.html', context)
+
+    def post(self, request, *args, **kwargs):
+        form = NewCategoryForm(request.POST or None)
+        context = {'form': form}
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect('/product-specs/')
+        return render(request, 'new_category.html', context)
+
+
+class NewFeatureView(View):
+
+    def get(self, request, *args, **kwargs):
+        form = NewCategoryFeatureKeyForm(request.POST or None)
+        context = {'form': form}
+        return render(request, 'new_feature.html', context)
+
+    def post(self, request, *args, **kwargs):
+        form = NewCategoryFeatureKeyForm(request.POST or None)
+        if form.is_valid():
+            new_category_featute_key = form.save(commit=False)
+            new_category_featute_key.category = form.cleaned_data['category']
+            new_category_featute_key.feature_name = form.cleaned_data['feature_name']
+            new_category_featute_key.save()
+            return HttpResponseRedirect('/product-specs/')
+        context = {'form': form}
+        return render(request, 'new_feature.html', context)
